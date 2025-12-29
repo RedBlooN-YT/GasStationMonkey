fbef2c9890fbcc77da2d3dab1e1925bceab43069
RedBlooN-YT <monkeyfiesta2@gmail.com>
2025-12-28 17:07:17 -0700

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,13 @@
+namespace GasStationMonkey;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "52.0";
+    public const string Version = "2.0.0";
+    public const string Name = "GasStationMonkey";
+
+    public const string Description = "Adds Gas Station Monkey to the game.";
+
+    public const string RepoOwner = "RedBlooN-YT";
+    public const string RepoName = "GasStationMonkey";
+}
